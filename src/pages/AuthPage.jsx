6fb908302aee1ba9b9580dd8ad0a3c8d5c1094f0
Abdi-01 <abdialghi@gmail.com>
2021-11-24 10:39:42 +0700

    ER-02 : show/hide password

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Button, Container, FormGroup, Input, Label } from 'reactstrap';
+import { Button, Container, FormGroup, Input, InputGroup, InputGroupText, Label } from 'reactstrap';
 
 const API_URL = "http://localhost:2000"
 
@@ -8,7 +8,11 @@ class AuthPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ""
+            email: "",
+            logPassShow: "Show",
+            logPassType: "password",
+            regPassShow: "Show",
+            regPassType: "password"
         }
     }
 
@@ -20,11 +24,39 @@ class AuthPage extends React.Component {
         // alert(`${this.state.email}, ${this.passwordLogin.value}`)
 
         axios.get(`${API_URL}/users?email=${this.state.email}&password=${this.passwordLogin.value}`)
-        .then((response)=>{
-            console.log(response.data)
-        }).catch((err)=>{
-            console.log(err)
-        })
+            .then((response) => {
+                console.log(response.data)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
+
+    btShowPassLogin = () => {
+        if (this.state.logPassType == "password") {
+            this.setState({
+                logPassShow: "Hide",
+                logPassType: "text"
+            })
+        } else {
+            this.setState({
+                logPassShow: "Show",
+                logPassType: "password"
+            })
+        }
+    }
+
+    btShowPassRegis = () => {
+        if (this.state.regPassType == "password") {
+            this.setState({
+                regPassShow: "Hide",
+                regPassType: "text"
+            })
+        } else {
+            this.setState({
+                regPassShow: "Show",
+                regPassType: "password"
+            })
+        }
     }
 
     render() {
@@ -42,14 +74,49 @@ class AuthPage extends React.Component {
                         </FormGroup>
                         <FormGroup>
                             <Label for="textPassword">Password</Label>
-                            <Input type="password" id="textPassword" placeholder="Masukkan Password Anda"
-                                innerRef={(element) => this.passwordLogin = element} />
+                            <InputGroup>
+                                <Input type={this.state.logPassType} id="textPassword" placeholder="Masukkan Password Anda"
+                                    innerRef={(element) => this.passwordLogin = element} />
+                                <InputGroupText style={{ cursor: "pointer" }} onClick={this.btShowPassLogin}>
+                                    {this.state.logPassShow}
+                                </InputGroupText>
+                            </InputGroup>
                         </FormGroup>
                         <Button color="primary" style={{ width: "100%" }} onClick={this.btLogin}>Masuk</Button>
                     </div>
-                    <div className="col-6">
+                    <div className="col-6 p-5">
                         <h3 className="text-center py-3">Silahkan buat akun anda</h3>
-
+                        <FormGroup>
+                            <Label for="textUsername">Username</Label>
+                            <Input type="text" id="textUsername" placeholder="Masukkan Username Anda"
+                                innerRef={(element) => this.usernameRegis = element} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="textEmail">Email</Label>
+                            <Input type="text" id="textEmail" placeholder="Masukkan Email Anda"
+                                innerRef={(element) => this.emailRegis = element} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="textPassword">Password</Label>
+                            <InputGroup>
+                                <Input type={this.state.regPassType} id="textPassword" placeholder="Masukkan Password Anda"
+                                    innerRef={(element) => this.passwordRegis = element} />
+                                <InputGroupText style={{ cursor: "pointer" }} onClick={this.btShowPassRegis}>
+                                    {this.state.regPassShow}
+                                </InputGroupText>
+                            </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="textPassword">Konfirmasi Password</Label>
+                            <InputGroup>
+                                <Input type={this.state.regPassType} id="textPassword" placeholder="Konfirmasi Password Anda"
+                                    innerRef={(element) => this.passwordRegis = element} />
+                                <InputGroupText style={{ cursor: "pointer" }} onClick={this.btShowPassRegis}>
+                                    {this.state.regPassShow}
+                                </InputGroupText>
+                            </InputGroup>
+                        </FormGroup>
+                        <Button color="primary" style={{ width: "100%" }} onClick={this.btLogin}>Daftar</Button>
                     </div>
                 </div>
             </Container>
