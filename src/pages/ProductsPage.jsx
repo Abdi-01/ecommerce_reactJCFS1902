4bbe20526293b2a62209d9c18283c7159d31e27b
Abdi-01 <abdialghi@gmail.com>
2021-12-01 15:27:30 +0700

    ER-21 : filter data

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ButtonGroup, Card, CardBody, CardImg, CardTitle, Input, InputGroup, InputGroupText } from 'reactstrap'
+import { Button, ButtonGroup, Card, CardBody, CardImg, CardTitle, FormGroup, Input, InputGroup, InputGroupText, Label, Spinner } from 'reactstrap'
 import { connect } from 'react-redux';
 import { getProductsAction } from '../redux/actions'
 import { Link } from 'react-router-dom';
@@ -7,21 +7,23 @@ class ProductsPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: 1
+            page: 1,
+            loading: true
         }
     }
 
     printProducts = () => {
         let { page } = this.state
         return this.props.productsList.slice(page > 1 ? (page - 1) * 8 : page - 1, page * 8).map((value, index) => {
-            return <div className="col-3 mt-2">
-                <Card>
+            return <div className="col-3 mt-3">
+                <Card className="shadow" style={{ border: "none" }}>
                     <Link to={`/product-detail?id=${value.id}`}
                         style={{ textDecoration: "none", color: "black" }}>
                         <CardImg top
                             src={value.images[0]}
                             width="100%"
                             alt={`${value.nama}-${index}`}
+                            className="shadow-sm"
                         />
                         <CardBody>
                             <CardTitle tag="h5" style={{ fontWeight: "bolder" }}>{value.nama}</CardTitle>
@@ -49,31 +51,57 @@ class ProductsPage extends React.Component {
     }
 
     btSearch = () => {
-        this.props.getProductsAction(this.inSearchName.value)
+        this.props.getProductsAction({
+            nama: this.inSearchName.value,
+            hargaMax: this.inSearchMax.value,
+            hargaMin: this.inSearchMin.value
+        })
         this.setState({ page: 1 })
     }
 
-    render() {
+    btReset = () => {
+        this.props.getProductsAction()
+        this.inSearchName.value = null
+        this.inSearchMax.value = null
+        this.inSearchMin.value = null
+    }
 
+    render() {
         return (
             <div className="pt-5">
                 <div className="container">
-                    <div style={{ display: "flex", justifyContent: "flex-end" }}>
-                        <InputGroup style={{ width: "350px" }}>
-                            <Input type="text" id="text" placeholder="Cari produk"
-                                innerRef={(element) => this.inSearchName = element} />
-                            <InputGroupText style={{ cursor: "pointer" }} onClick={this.btSearch}>
-                                Search
-                            </InputGroupText>
-                        </InputGroup>
-                        <Input type="select" style={{ width: "250px" }}>
-                            <option value="harga-asc">Harga Asc</option>
-                            <option value="harga-desc">Harga Desc</option>
-                            <option value="nama-asc">A-Z</option>
-                            <option value="nama-desc">Z-A</option>
-                            <option value="id-asc">Reset</option>
-                        </Input>
-                        <Button outline color="warning" onClick={()=>this.props.getProductsAction()}>Reset</Button>
+                    <div className="shadow bg-white p-2 rounded mb-3">
+                        <div style={{ display: "flex", justifyContent: "space-around" }}>
+                            <FormGroup>
+                                <Label>Nama</Label>
+                                <Input type="text" id="text" placeholder="Cari produk"
+                                    innerRef={(element) => this.inSearchName = element} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Harga</Label>
+                                <div className="d-flex">
+                                    <Input type="number" id="numb1" placeholder="Minimum"
+                                        innerRef={(element) => this.inSearchMin = element} />
+                                    <Input type="number" id="numb2" placeholder="Maksimum"
+                                        innerRef={(element) => this.inSearchMax = element} />
+                                </div>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Sort</Label>
+                                <Input type="select" style={{ width: "250px" }}>
+                                    <option value="harga-asc">Harga Asc</option>
+                                    <option value="harga-desc">Harga Desc</option>
+                                    <option value="nama-asc">A-Z</option>
+                                    <option value="nama-desc">Z-A</option>
+                                    <option value="id-asc">Reset</option>
+                                </Input>
+                            </FormGroup>
+
+                        </div>
+                        <div className="pt-2" style={{ textAlign: "end" }}>
+                            <Button outline color="warning" onClick={this.btReset}>Reset</Button>
+                            <Button style={{ marginLeft: 16 }} color="primary" onClick={this.btSearch}>Filter</Button>
+                        </div>
                     </div>
                     <div className="row">
                         {this.printProducts()}
--- src/redux/actions/productsAction.js
+++ src/redux/actions/productsAction.js
@@ -6,9 +6,17 @@ export const getProductsAction = (search = null) => {
         try {
             let res;
             // cara 1
-            console.log("cek",search)
+            console.log("cek", search)
             if (search) {
-                res = await axios.get(`${API_URL}/products?nama=${search}`)
+                if (search.nama) {
+                    if (search.hargaMax > 0 && search.hargaMin > 0) {
+                        res = await axios.get(`${API_URL}/products?harga_gte=${search.hargaMin}&harga_lte=${search.hargaMax}&nama=${search.nama}`)
+                    } else {
+                        res = await axios.get(`${API_URL}/products?nama=${search.nama}`)
+                    }
+                } else {
+                    res = await axios.get(`${API_URL}/products?harga_gte=${search.hargaMin}&harga_lte=${search.hargaMax}`)
+                }
             } else {
                 console.log("cek")
                 res = await axios.get(`${API_URL}/products`)
